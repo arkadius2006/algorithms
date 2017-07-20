df5cf2e4e114b17cce00dd7d01044e2a683ed506
Arkadiy Seletskiy <arkadius2006@gmail.com>
2017-07-21 01:18:30 +0300

    D-27: Knuth references

--- src/main/java/com/math/data/Queue.java
+++ src/main/java/com/math/data/Queue.java
@@ -1,5 +1,14 @@
 package com.math.data;
 
+/**
+ * A queue is a linear list for which all insertions are made at one end of the list; all deletions (and accesses)
+ * are made from the other end.
+ * <p/>
+ * With queues we speak of the front and the rear of the queue, also known as head and tail. Things enter at the rear,
+ * and ultimatelly are removed when they reach the front position.
+ * <p/>
+ * Queues feature the so called first-in-first-out "FIFO" behavior.
+ */
 public interface Queue {
 
     boolean isEmpty();
--- src/main/java/com/math/data/Stack.java
+++ src/main/java/com/math/data/Stack.java
@@ -1,5 +1,13 @@
 package com.math.data;
 
+/**
+ * A stack is a linear list for which all insertions and deletions (and accesses) are made at one end of the list.
+ * <p/>
+ * We put item onto top of a stack, or take the top item off. The bottom of the stack is the least accessible item,
+ * and it will not be removed until all other items have been deleted.
+ * <p/>
+ * Stack features the so called last-in-first-out ("LIFO") behaviour.
+ */
 public interface Stack {
 
     boolean isEmpty();
