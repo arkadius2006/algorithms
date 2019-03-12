8fce79856890087bdab4cdb351ce35ba88c6a02c
Arkady Seletsky <arkady.seletsky@gmail.com>
2019-03-12 15:55:35 +0300

    fixed description

--- src/main/java/com/knuth/tree/Item.java
+++ src/main/java/com/knuth/tree/Item.java
@@ -6,10 +6,6 @@ package com.knuth.tree;
  * As opposed to {@link java.util.Optional}, value COULD be null.
  */
 public class Item {
-    private final boolean empty;
-    private final Object value;
-
-    private static Item EMPTY = new Item();
 
     public static Item empty() {
         return EMPTY;
@@ -17,16 +13,6 @@ public class Item {
 
     public static Item of(Object o) {
         return new Item(o);
-    }
-
-    private Item() {
-        this.empty = true;
-        this.value = null;
-    }
-
-    private Item(Object value) {
-        this.empty = false;
-        this.value = value;
     }
 
     public boolean isEmpty() {
@@ -39,5 +25,20 @@ public class Item {
         }
 
         return value;
+    }
+
+    private static Item EMPTY = new Item();
+
+    private final boolean empty;
+    private final Object value;
+
+    private Item() {
+        this.empty = true;
+        this.value = null;
+    }
+
+    private Item(Object value) {
+        this.empty = false;
+        this.value = value;
     }
 }
--- src/main/java/com/knuth/tree/TreeSequencer.java
+++ src/main/java/com/knuth/tree/TreeSequencer.java
@@ -1,13 +1,13 @@
 package com.knuth.tree;
 
 /**
- * Yields all tree nodes one by one.
+ * Generates all tree nodes one by one.
  * <p/>
  * This is an alternative interface for tree traversal algorithm.
  * It is more flexible (and powerful) then {@link TreeTraversal}.
  * <p/>
- * Likewise, any implementation of this interface produces
- * some linear ordering of tree nodes.
+ * Essentially, an algorithm that implements this interface
+ * produces some linear ordering of tree nodes.
  */
 public interface TreeSequencer {
 
