dbf86f99c5fd81a12f8cdfb38685b68b35126e41
Arkady Seletsky <arkady.seletsky@gmail.com>
2019-03-21 05:33:13 +0300

    post order tree traversal

---
+++ src/main/java/com/knuth/tree/impl/PostOrderTreeTraversal.java
@@ -0,0 +1,19 @@
+package com.knuth.tree.impl;
+
+import com.knuth.tree.Action;
+import com.knuth.tree.Node;
+import com.knuth.tree.TreeTraversal;
+
+/**
+ * Visits all tree nodes, each exactly one time, and performs an action at each node.
+ * <p/>
+ * Parent nodes are visited BEFORE children ("post-order").
+ */
+
+public class PostOrderTreeTraversal implements TreeTraversal {
+
+    @Override
+    public void traverse(Node n, Action a) {
+
+    }
+}
