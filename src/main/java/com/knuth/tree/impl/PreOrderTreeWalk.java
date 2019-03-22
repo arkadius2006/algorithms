8b6bd68c41a22fdd9c5e0bae9dcf6a590dd338f7
Arkady Seletsky <arkady.seletsky@gmail.com>
2019-03-22 23:32:15 +0300

    pre-order tree walk

--- src/main/java/com/knuth/tree/impl/PreOrderTreeWalk.java
+++ src/main/java/com/knuth/tree/impl/PreOrderTreeWalk.java
@@ -11,7 +11,7 @@ import com.knuth.tree.TreeWalk;
  * Visits all tree nodes, each exactly one time, and performs an action at each node.
  * <p/>
  * Parent nodes are visited <em>before</em> children ("pre-order").
- *
+ * <p>
  * This is explicit version of the following recursive algorithm:
  * <pre>
  * void R(Tree t, Action a) {
@@ -25,22 +25,21 @@ import com.knuth.tree.TreeWalk;
  */
 public class PreOrderTreeWalk implements TreeWalk {
 
-    public void walk(Tree t, Action a) {
+    @Override
+    public void walk(Tree a, Action q) {
         Stack s = new LinkedStack();
 
-        while (t != null || !s.isEmpty()) {
-            if (t != null) {
-                a.act(t);
-                s.push(t);
-                t = t.left();
+        while (true) {
+            if (a != null) {
+                q.act(a);
+                a = a.left();
+                s.push(a.right());
+            } else if (!s.isEmpty()) {
+                a = (Tree) s.pop();
             } else {
-                t = (Tree) s.pop();
-                t = t.right();
+                break;
             }
         }
     }
 
-    private void walk() {
-
-    }
 }
