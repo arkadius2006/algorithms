15ff7c8e292c8657f6dc8b68613604d4a96f26ad
Arkady Seletsky <arkady.seletsky@gmail.com>
2020-11-14 20:49:20 +0300

    problem statement

---
+++ src/main/java/com/knuth/tsorting/TopologicalSorting.java
@@ -0,0 +1,13 @@
+package com.knuth.tsorting;
+
+/**
+ * Given digraph, arrange graph nodes in sequence
+ * <pre>
+ * a[1], a[2], ..., a[n]
+ * </pre>
+ * such that for each edge (a -> b) in digraph node a precedes node b in the above sequence.
+ * <p/>
+ * In other words a[i] -> a[j] implies i < j.
+ */
+public interface TopologicalSorting {
+}
