a88ed4e8cd89f7e6b2828834b38a5e594be106f2
Arkady Seletsky <arkadius2006@gmail.com>
2022-10-18 12:55:20 +0300

    Tinkoff max sequence: make code compile, add class

--- src/main/java/com/tinkoff/MaxSequence.java
+++ src/main/java/com/tinkoff/MaxSequence.java
@@ -1,39 +1,44 @@
-int computeMaxSequence(int[] a) {
-    // init
-    State state = State.C;
-    int count = 0;
-    int prev_count = 0;
-    int max = 0;
+package com.tinkoff;
 
-    // read each bit in turn, update FSM state and counts
-    for (int i = 0; i < a.length; i++) {
-        if (a[i] == 1) {    // ..1
-            count++;
+public class MaxSequence {
 
-            if (count + prev_count > max) {
-                max = count + prev_count;
+    int computeMaxSequence(int[] a) {
+        // init
+        State state = State.C;
+        int count = 0;
+        int prev_count = 0;
+        int max = 0;
+
+        // read each bit in turn, update FSM state and counts
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 1) {    // ..1
+                count++;
+
+                if (count + prev_count > max) {
+                    max = count + prev_count;
+                }
+
+                state = State.A;
+
+                // a[i] == 0 from here
+            } else if (state == State.A) {  // ..10
+                prev_count = count;
+                count = 0;
+                state = State.B;
+            } else if (state == State.B) {   // ..00
+                prev_count = 0;
+                state = State.C;
             }
-
-            state = state.A;
-
-        // a[i] == 0 from here
-        } else if (state == State.A) {  // ..10
-            prev_count = count;
-            count = 0;
-            state = state.B;
-        } else if (state = State.B) {   // ..00
-            prev_count = 0;
-            state = State.C;
+            // other cases: no action
         }
-        // other cases: no action
+
+        return max;
     }
 
-    return max;
-}
-
-// FSM states
-enum State {
-    A, // seen ...1 before
-    B, // seen ..10 before
-    C  // seen ..00 before or start of sequence
+    // FSM states
+    enum State {
+        A, // seen ...1 before
+        B, // seen ..10 before
+        C  // seen ..00 before or start of sequence
+    }
 }
